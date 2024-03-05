c1f8079f3dff87e6892cecd5163e5f952513fa66
baeksu <kyungwoo5826@naver.com>
2024-03-05 23:03:54 +0800

    try to get stock info from dart openapi

---
+++ index.js
@@ -0,0 +1,13 @@
+// import 랑 require 랑 차이가 뭐지
+import axios from "axios";
+
+const express = require("express");
+const app = express();
+
+//금융감독원 openapi key
+const openDartApiKey = "69872aa9aab15c6390ed38b51202b0021295cc12";
+
+console.log("start");
+const res = axios.get();
+
+console.log("end");
